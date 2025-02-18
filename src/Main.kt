d758f9802a8296d0a23cc0c60fce9da01fecd336
abdulwahab alsenan <ak-alsenan@hotmail.com>
2025-02-18 18:32:03 +0300

    v1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,23 @@
 fun main() {
-    println("Hello World!")
+    var age = 0
+//1.2   age = null
+
+    //2
+    var city:String? = "Tokyo"
+    city = null
+
+    //3
+    println(city?.length)
+
+    //4
+    city?:"default city" //no null allowed >:(
+
+    //5.2
+    doubleOrNull("Abdulwahab")
+    doubleOrNull(null)
+}
+
+//5.1
+fun doubleOrNull(isItADouble:String?){
+   println(isItADouble?.reversed())
 }
