26df5ff78dfa417b7a0b8a2d27102c70a8695463
abdulwahab alsenan <ak-alsenan@hotmail.com>
2025-02-19 12:29:49 +0300

    v2

--- src/Main.kt
+++ src/Main.kt
@@ -5,19 +5,18 @@ fun main() {
     //2
     var city:String? = "Tokyo"
     city = null
-
     //3
-    println(city?.length)
+    println(city?.length ?:"Nulladelphia")
 
     //4
     city?:"default city" //no null allowed >:(
 
     //5.2
-    doubleOrNull("Abdulwahab")
+    doubleOrNull(3.14)
     doubleOrNull(null)
 }
 
 //5.1
-fun doubleOrNull(isItADouble:String?){
-   println(isItADouble?.reversed())
+fun doubleOrNull(isItADouble:Double?){
+    println(isItADouble?.toInt()            /*if null*/ ?: "Null passed  instead of double")
 }
